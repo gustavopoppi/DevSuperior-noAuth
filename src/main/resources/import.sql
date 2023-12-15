c8e964e6d1d18ebdaf91e96e0e7df2d607e20003
Gustavo Poppi <gustavopoppi@hotmail.com>
2023-12-15 14:29:19 -0300

    [DEV] Altera senha para encriptada do seed de dados dos usuários;

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$12$jyZm9YPeAU5h6ine63ZX1O.yYry1m/vpX75gJZjNJZ6ViJrdeS4i6');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$12$jyZm9YPeAU5h6ine63ZX1O.yYry1m/vpX75gJZjNJZ6ViJrdeS4i6');
 
 INSERT INTO tb_product (name) VALUES ('TV');
 INSERT INTO tb_product (name) VALUES ('Computer');
